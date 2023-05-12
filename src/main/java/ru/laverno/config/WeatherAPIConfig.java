64ee923dfe38430ab363b7b2faa45982d0553e7a
MakarovMD <OrizonJust@gmail.com>
2023-05-12 08:35:23 +0300

    feature/hourly_weather - Добавлена защита для API ключа

---
+++ src/main/java/ru/laverno/config/WeatherAPIConfig.java
@@ -0,0 +1,15 @@
+package ru.laverno.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Getter
+@Configuration
+@PropertySource(value = "classpath:application.yml")
+public class WeatherAPIConfig {
+
+    @Value("${weather.key}")
+    private String key;
+}
